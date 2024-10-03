f4bc6c4286f37f42ffe24ac67302f0f1954e286a
rintarotajima <rintarotajima@gmail.com>
2024-10-03 15:02:57 +0900

    [update] index.cssの代わりにstyle.cssをimport

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './style.css'
 import { Todo } from './Todo'
 
 createRoot(document.getElementById('root')).render(
