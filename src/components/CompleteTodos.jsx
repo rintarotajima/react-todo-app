1afdd346c3058778a28ff49e59b79b9e90a52143
rintarotajima <rintarotajima@gmail.com>
2024-10-04 14:48:54 +0900

    [update] CompleteTodos.jsx内でスタイルを定義

--- src/components/CompleteTodos.jsx
+++ src/components/CompleteTodos.jsx
@@ -1,18 +1,28 @@
+const style = {
+  backgroundColor: "#63c57b",
+  width: "500px",
+  minHeight: "250px",
+  padding: "10px",
+  margin: "10px",
+  border: "2px solid",
+  borderRadius: "5px",
+};
+
 export const CompleteTodos = (props) => {
-    const { todos, onClick } = props;
-    return (
-        <div className="complete-area">
-        <p className="title">完了のTodo</p>
-        <ul>
-          {todos.map((todo, index) => (
-            <li key={todo}>
-              <div className="list-row">
-                <p>{todo}</p>
-                <button onClick={() => onClick(index)}>戻す</button>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-}
+  const { todos, onClick } = props;
+  return (
+    <div style={style}>
+      <p className="title">完了のTodo</p>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={todo}>
+            <div className="list-row">
+              <p>{todo}</p>
+              <button onClick={() => onClick(index)}>戻す</button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
